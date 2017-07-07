77c80c5a2444956dff3121c22ab4c5bfa91b8e81
Atishya Jain <atishya.jain@rocketmail.com>
2017-07-07 14:15:10 +0530

    Multiplayer connections done

---
+++ public/js/OnlineGamePlay.js
@@ -0,0 +1,60 @@
+// JavaScript Document for main Play
+/**************************************************
+** Controller functions for the game
+**************************************************/
+
+var alreadygenerated = 0;
+var iterationTime = 10; // in milis
+
+
+  
+function playGame() {
+	// function which controls the game 
+	// called repeatedly after each 'iterationTime' milisecond
+	 
+	 moveTank(theTank);
+	 drawmaze();
+	 shootTank(theTank);
+	 update();
+	 
+	 var i;
+	 for (i = 0; i < remotePlayers.length; i++) {
+			drawTank(remotePlayers[i], "orange", "red", "yellow", "#E3EF1E");
+		 	for(var bulletCount = 0; bulletCount < remotePlayers[i].bullet.length; bulletCount++){
+				// draw the bullets
+				drawBullet(remotePlayers[i].bullet[bulletCount]);
+			}
+		};
+	myScore.text="SCORE: " + 7;
+    myScore.update();
+	changeHealth();
+}
+
+async function generate() {
+	if(alreadygenerated == 0){
+	drawmaze();
+	alreadygenerated++;
+	}
+	theTank = new Tank();
+	for (var i = 0; i < theTank.bulletPack; i++)
+    	theTank.bullet.push(new Bullet());
+
+	if (onceLoaded == 0) onceLoaded++;
+	init();
+	
+	setEventHandlers();
+	// caution: this time depends on network speed
+	//          when moving to actual online server test and increase this time to avoid previous
+	//          'double start' errors
+	await sleep(50);
+	initialize();
+	//console.log("after maze.initialize : tankCenterX = "+ theTank.tankCenterX);
+	if (loaded == 0){
+		setInterval(playGame, iterationTime);
+	}
+
+	loaded++;
+}
+function reSetTank() {
+	initialize();
+}
--- server.js
+++ server.js
@@ -1,35 +1,170 @@
-'use strict';
 
-const util = require("util");
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var util = require('util');
+var players = [];
+var Player = require("./js/Player");
+app.use(express.static("public"));
+app.set("view engine","ejs");
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected' + socket.id);
-  util.log("New player has connected: " + socket.id);
-  /*socket.on('disconnect', () => {
-	  console.log('Client disconnected');
-	  util.log("Player has disconnected: "+socket.id+"server.js message");
-  });*/
+app.get("/",function(req,res){
+  res.render("index");
+});
+app.get("/Player1.ejs",function(req,res){
+  res.render("Player1");
+});
+app.get("/Player2.ejs",function(req,res){
+  res.render("Player2");
+});
+app.get("/OnlinePlay.ejs",function(req,res){
+  res.render("OnlinePlay");
+});
+app.get("/LocalPlay.ejs",function(req,res){
+  res.render("LocalPlay");
 });
 
-/*io.on('disconnect', (socket) => {
-  	console.log('Client disconnected');
-	util.log("Player has disconnected: "+socket.id+"server.js message");
+/*io.on('connection', function(socket) {  
+    console.log('Client connected...' + socket.id);
 });*/
 
+io.on('connection', function(client){
+  console.log('Client connected' + client.id);
+  util.log("New player has connected: " + client.id);
+  
+  /*Client.on('disconnect', function(){
+    console.log('Client disconnected');
+    util.log("Player has disconnected: "+Client.id+"server.js message");
+  });*/
+
+  // Maze design
+  this.emit("Player", {
+    rows: Player.rows,
+    columns: Player.columns,
+    backgroundColor: Player.backgroundColor,
+    wallColor: Player.wallColor,
+    grid: Player.grid,
+    mazeHeight: Player.mazeHeight
+  });
+
+  
+  //client disconnected
+  client.on("disconnect", onClientDisconnect);
+
+  // new player message
+  client.on("new player", onNewPlayer);
+
+  // move player message
+  client.on("move player", onMovePlayer);
+  
+});
+
+// Socket client has disconnected
 function onClientDisconnect() {
-	util.log("Player has disconnected: "+socket.id+"server.js message");
+  util.log("Player has disconnected: "+this.id+"server.js message");
+
+  var removePlayer = playerById(this.id);
+
+  // Player not found
+  if (!removePlayer) {
+    util.log("Player not found: "+this.id+"server.js message");
+    return;
+  };
+
+  // Remove player from players array
+  players.splice(players.indexOf(removePlayer), 1);
+
+  // Broadcast removed player to connected socket clients
+  this.broadcast.emit("remove player", {id: this.id});
 };
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// New player has joined
+function onNewPlayer(data) {
+  // Create a new player
+  var newPlayer = new Player.Player(); // same as new Tank();
+  newPlayer.id = this.id;
+  Player.Tank(newPlayer); // Same as initializeTank(aTank)
+
+  // We had to use this new way of initialize because of require and exports method of using functions of other javascript.
+
+  // Broadcast new player to connected socket clients
+  this.broadcast.emit("new player", {
+    id: newPlayer.id,
+    x: newPlayer.tankCenterX,
+    y: newPlayer.tankCenterY,
+    rotorX: newPlayer.rotorX,
+    rotorY: newPlayer.rotorY,
+    angle: newPlayer.rotorAngle,
+    bulletArray: newPlayer.bullet
+  });
+
+  // Send existing players to the new player
+  var i, existingPlayer;
+  for (i = 0; i < players.length; i++) {
+    existingPlayer = players[i];
+    this.emit("new player", {
+      id: existingPlayer.id,
+      x: existingPlayer.tankCenterX,
+      y: existingPlayer.tankCenterY,
+      rotorX: existingPlayer.rotorX,
+      rotorY: existingPlayer.rotorY,
+      angle: existingPlayer.rotorAngle,
+      bulletArray: existingPlayer.bullet
+    });
+  };
+  // Add new player to the players array
+  players.push(newPlayer);
+};
+
+// Player has moved
+function onMovePlayer(data) {
+  // Find player in array
+  var movePlayer = playerById(this.id);
+
+  // Player not found
+  if (!movePlayer) {
+    util.log("Player not found: "+this.id+"server.js message");
+    return;
+  };
+
+  // Update player position
+  movePlayer.tankCenterX = data.x;
+  movePlayer.tankCenterY = data.y;
+  movePlayer.rotorX = data.rotorX;
+  movePlayer.rotorY = data.rotorY;
+  movePlayer.rotorAngle = data.angle;
+  movePlayer.bullet = data.bulletArray;
+
+  // Broadcast updated position to connected socket clients
+  this.broadcast.emit("move player", {
+    id: movePlayer.id,
+    x: movePlayer.tankCenterX,
+    y: movePlayer.tankCenterY,
+    rotorX: movePlayer.rotorX,
+    rotorY: movePlayer.rotorY,
+    angle: movePlayer.rotorAngle,
+    bulletArray: movePlayer.bullet
+  });
+};
+
+
+/**************************************************
+** GAME HELPER FUNCTIONS
+**************************************************/
+// Find player by ID
+function playerById(id) {
+  var i;
+  for (i = 0; i < players.length; i++) {
+    if (players[i].id == id)
+      return players[i];
+  }
+
+  return false;
+};
+
+
+server.listen(port,function(){
+  console.log("listening");
+});
